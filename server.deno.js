2643402dce56347c94183b612c1f73cebb8f01fa
20249NakanihiRyo <113967335+20249NakanishiRyo@users.noreply.github.com>
2023-08-18 17:29:09 +0900

    Add:ログイン・新規登録機能を追加
    db-controller.jsを追加
    index.htmlにlogin.htmlへのリンクを追加
    login.htmlを追加
    newuser.htmlを追加
    server.deno.jsにAPIを追加
    README.mdにAPI構成を追加

--- server.deno.js
+++ server.deno.js
@@ -1,12 +1,79 @@
-import { serve } from "https://deno.land/std@0.151.0/http/server.ts";
-import { serveDir } from "https://deno.land/std@0.151.0/http/file_server.ts";
+import { serveDir } from "https://deno.land/std@0.180.0/http/file_server.ts";
+import { serve } from "https://deno.land/std@0.180.0/http/server.ts";
+import { DIDAuth } from 'https://jigintern.github.io/did-login/auth/DIDAuth.js';
+import { addDID, checkIfIdExists, getUser } from './db-controller.js';
 
 serve(async (req) => {
   const pathname = new URL(req.url).pathname;
   console.log(pathname);
 
-  if (req.method === "GET" && pathname === "/welcome-message") {
-    return new Response("jigインターンへようこそ！");
+  if (req.method === "POST" && pathname === "newuser") {
+    const json = await req.json();
+    const userName = json.name;
+    const sign = json.sign;
+    const did = json.did;
+    const message = json.message;
+
+    // 電子署名が正しいかチェック
+    try {
+      const chk = DIDAuth.verifySign(did, sign, message);
+      if (!chk) {
+        return new Response("不正な電子署名です", { status: 400 });
+      }
+    } catch (e) {
+      return new Response(e.message, { status: 500 });
+    }
+
+    // 既にDBにDIDが登録されているかチェック
+    try {
+      const isExists = await checkIfIdExists(did);
+      if (isExists) {
+        return Response("登録済みです", { status: 400 });
+      }
+    } catch (e) {
+      return new Response(e.message, { status: 500 });
+    }
+
+    // DBにDIDとuserNameを保存
+    try {
+      await addDID(did, userName);
+      return new Response("ok");
+    } catch (e) {
+      return new Response(e.message, { status: 500 });
+    }
+  }
+
+  if (req.method === "POST" && pathname === "/login") {
+    const json = await req.json();
+    const sign = json.sign;
+    const did = json.did;
+    const message = json.message;
+  
+    // 電子署名が正しいかチェック
+    try {
+      const chk = DIDAuth.verifySign(did, sign, message);
+      if (!chk) {
+        return new Response("不正な電子署名です", { status: 400 });
+      }
+    } catch (e) {
+      return new Response(e.message, { status: 400 });
+    }
+  
+    // DBにdidが登録されているかチェック
+    try {
+      const isExists = await checkIfIdExists(did);
+      if (!isExists) {
+        return new Response("登録されていません", { status: 400 });
+      }
+      // 登録済みであればuser情報を返す
+      const res = await getUser(did);
+      const user = { did: res.rows[0].did, name: res.rows[0].user_name };
+      return new Response(JSON.stringify({ user }), {
+        headers: { "Content-Type": "application/json" },
+      });
+    } catch (e) {
+      return new Response(e.message, { status: 500 });
+    }
   }
 
   return serveDir(req, {
@@ -16,3 +83,4 @@ serve(async (req) => {
     enableCors: true,
   });
 });
+
