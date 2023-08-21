1652dd765f4802e0cbb7dd898211b64323cbcb54
20249NakanihiRyo <113967335+20249NakanishiRyo@users.noreply.github.com>
2023-08-21 15:43:57 +0900

    Fix:プルした
    ブランチを切った後プルしてなかったのでした

--- server.deno.js
+++ server.deno.js
@@ -1,7 +1,7 @@
 import { serveDir } from "https://deno.land/std@0.180.0/http/file_server.ts";
 import { serve } from "https://deno.land/std@0.180.0/http/server.ts";
 import { DIDAuth } from 'https://jigintern.github.io/did-login/auth/DIDAuth.js';
-import { addDID, checkIfIdExists, getUser, addPost, getPost, getPosts_index } from './db-controller.js';
+import { addDID, checkIfIdExists, getUser, addPost, getPost, delPost, fixPost, isPostExists, getPosts_index } from './db-controller.js';
 
 serve(async (req) => {
   const pathname = new URL(req.url).pathname;
